8dcd63d2508c0fc3cae75118195e0976fd63bf0d
Runebreaker <gino.schulze-skyfire@web.de>
2024-09-04 19:26:29 +0200

    Dropdown stuff testimplementation

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
+import PIL.Image
 import gradio as gr
 
-hello_world = gr.Interface(lambda name: "Hello " + name, "text", "text")
-bye_world = gr.Interface(lambda name: "Bye " + name, "text", "text")
+presets = {}
+encoded_text = []
 
-demo = gr.TabbedInterface([hello_world, bye_world], ["Hello World", "Bye World"])
+
+def add_symbol(input_img: PIL.Image.Image):
+    pass
+
+
+typing_interface = gr.Interface(lambda name: "Hello " + name, "text", "text")
+substitution_interface = gr.Interface(lambda name: "Bye " + name, "text", "text")
+with gr.Blocks() as image_interface:
+    gr.Dropdown(list(presets.keys()), label="New Symbol", info="Upload an image of a symbol to add it to the current key set.")
+    gr.Image()
+    gr.Button("Add")
+
+demo = gr.TabbedInterface([typing_interface, substitution_interface, image_interface], ["Hello World", "Bye World", "Alphabets"])
 
 if __name__ == "__main__":
     demo.launch()
