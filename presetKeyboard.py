44924637422a11c781e6fad8965e4dbf58be9f9f
Phil <philine.bargen@stud-mail.uni-wuerzburg.de>
2024-09-04 22:10:02 +0200

    started preset classes

---
+++ presetKeyboard.py
@@ -0,0 +1,3 @@
+class Keyboard:
+    def __init__(self, symbol_list):
+        self.list = symbol_list
---
+++ presetSymbol.py
@@ -0,0 +1,5 @@
+class Symbol:
+    def __init__(self, id, image):
+      self.id = id
+      self.image = image
+
